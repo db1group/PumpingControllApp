3d1dbf08fd7cf76c29491b3c02feb2339c602c10
Lucas Andrew <lukas29pr@gmail.com>
2023-10-09 10:32:09 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pumping-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Pumping Code';
+}
